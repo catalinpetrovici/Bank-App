267eb31a71a6f61ed94d651f1dfa4441636025ea
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-03-19 19:09:56 +0200

    Sticky navigation with scroll event but this method is not really efficient and usually it should be avoided

--- WebSite/script.js
+++ WebSite/script.js
@@ -97,3 +97,15 @@ const handleHover = function (event) {
 // Passing "argument" into handler
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky navigation
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll', () => {
+  // scroll event is not really efficient and usually it should be avoided
+  console.log(window.scrollY);
+
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
