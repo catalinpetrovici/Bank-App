86af9130475a847e8da74fee208d6e21dd227520
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-03-19 18:06:51 +0200

    Menu fade animation / Passing Arguments to Event Handlers

--- WebSite/script.js
+++ WebSite/script.js
@@ -10,6 +10,12 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+const nav = document.querySelector('.nav');
+
 const openModal = function (event) {
   event.preventDefault();
   modal.classList.remove('hidden');
@@ -49,11 +55,7 @@ btnScrollTo.addEventListener('click', () => {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-// Tabbed component
-
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
+// Tabbed component // Event delegation
 
 tabsContainer.addEventListener('click', (event) => {
   const clicked = event.target.closest('.operations__tab');
@@ -72,3 +74,26 @@ tabsContainer.addEventListener('click', (event) => {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// Menu fade animation // Event delegation
+
+const handleHover = function (event) {
+  // mouseover is similar to mouseenter
+  // but mouseenter does not bubble
+  if (event.target.classList.contains('nav__link')) {
+    const link = event.target;
+
+    // Select the other links
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
